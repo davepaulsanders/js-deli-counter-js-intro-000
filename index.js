afba11321f50ddc5a7d50bb8fd04d8b39d50f2b1
David P Sanders <davepaulsanders@gmail.com>
2021-04-03 15:27:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ katzLine.push(name)
   return `Welcome, ${name}. You are number ${katzLine.length} in line.`
 
 }
-// maybe I need a counter to give each name it's own index?  I thought they should index automatically but I'm not sure
-// I think you would need a while loop, because you don't actually know how many they are going to add
 
 function currentLine() {
   if (katzLine.length >= 1) {
