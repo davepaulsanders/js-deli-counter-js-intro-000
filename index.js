55972bea4947f7ad2f709a31f636f03dd52f8113
David P Sanders <davepaulsanders@gmail.com>
2021-04-03 15:34:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ katzLine.push(name)
 }
 
 function currentLine(katzLine) {
-  if (katzLine.length >= 1) {
+  while (katzLine.length >= 1) {
   return `The line is currently: $[i]. ${katzLine[i]}`
-} else {
+}
   return "The line is currently empty."
 }
 }
